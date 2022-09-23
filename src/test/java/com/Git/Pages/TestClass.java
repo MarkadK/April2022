6724a0ee584e6c6f80e83846f8a31be07f66a789
Kishor <kishormarkad2511@gmail.com>
2022-09-23 09:12:05 +0530

    This is my commit

---
+++ src/test/java/com/Git/Pages/TestClass.java
@@ -0,0 +1,13 @@
+package com.Git.Pages;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+	
+	@Test
+	public void testCase1()
+	{
+		System.out.println("Hello Good Morning GitHub");
+	}
+
+}
